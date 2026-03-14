c8ce9d04420b0d9e2a17899d43aaf487807432ea
hsteinmueller <harald.steinmueller@gmail.com>
2026-03-14 12:26:59 +0100

    add trigger (does not work)

---
+++ src/Trigger.js
@@ -0,0 +1,8 @@
+/**
+ * Creates two time-driven triggers.
+ * @see https://developers.google.com/apps-script/guides/triggers/installable#time-driven_triggers
+ */
+function createTimeDrivenTriggers() {
+  // Trigger every 6 hours.
+  ScriptApp.newTrigger("myFunction").timeBased().atHour(0).create();
+}
